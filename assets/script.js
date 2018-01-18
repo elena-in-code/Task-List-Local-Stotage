7acdf7fbbd6f7cb4a3efedf89b0b086f41939ffe
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-01-18 11:53:07 +0100

    remove task and clear all tasks

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,28 @@
 //UI variables:
+    //form
 const form = document.querySelector("#task-form");
+    //ul
 const taskList = document.querySelector(".collection");
+    //delete ul btn
 const clearBtn = document.querySelector(".clear-tasks");
+    //input filter
 const filter = document.querySelector("#filter");
+    // task input
 const taskInput = document.querySelector("#task");
 
 // Invoke load all event listeners function:
 loadEventListeners();
+
 // load all event listeners function:
 function loadEventListeners(){
     //add task event:
     form.addEventListener("submit", addTask);
+    //remove task event:
+    taskList.addEventListener("click", removeTask);
+    //clear all task from ul event:
+    clearBtn.addEventListener("click", clearTasks);
 }
+
 // function add task:
 function addTask(e){
     if(taskInput.value === ""){
@@ -37,5 +48,23 @@ function addTask(e){
 
     //clear input
     taskInput.value = " ";
+    e.preventDefault();
+}
+
+//function remove task:
+function removeTask(e){
+    if(e.target.parentElement.classList.contains("delete-item")){
+        if(confirm("Are you sure you want to delete this task?")){
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+    e.preventDefault();
+}
+
+//function clear task list
+function clearTasks(e){
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
     e.preventDefault();
 }
